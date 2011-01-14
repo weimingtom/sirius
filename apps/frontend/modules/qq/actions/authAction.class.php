660240e6b5155e32a1e0452a8e76a1a08c5ffbec
getcary <getcary@gmail.com>
2011-01-14 07:33:47 +0000

    improvement: tweet sent date display BUG-FIX: QQ auth incorrectly improvement: set default timezone 'Asia/Chongqing' improvement: make scrollbar of thread panel shown automatically (not shown anyway)

--- apps/frontend/modules/qq/actions/authAction.class.php
+++ apps/frontend/modules/qq/actions/authAction.class.php
@@ -3,7 +3,7 @@
 class authAction extends QQAction {
 	public function execute($request) {
 		$to = new QQOAuth($this->consumerKey, $this->consumerSecret);
-		$tok = $to->getRequestToken($callback_url); 
+		$tok = $to->getRequestToken($this->callbackUrl); 
 		$this->getUser()->setAttribute("QQOAuthToken", $tok);
 		$request_link = $to->getAuthorizeURL($tok['oauth_token'], false, $this->callbackUrl);
 		return $this->redirect($request_link);		
