76aa5ac26eb11c4f4f1bf5cca3429c834a03390f
getcary <getcary@802b8065-c0c6-a8c3-afa2-0653f63199a3>
2011-01-16 14:01:52 +0000

    add google analytics script snippet

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -10,5 +10,17 @@
   </head>
   <body>
     <?php echo $sf_content ?>
+    <script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-20764783-1']);
+	  _gaq.push(['_setDomainName', '.mixmes.com']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>
   </body>
 </html>
